73d5eeabbfa1ef9844f780d69d51a110b2b46cbb
LucasLK <122848089+LKzinxyz@users.noreply.github.com>
2023-06-03 14:45:47 -0300

    1.9 NEW

--- Start.bat
+++ Start.bat
@@ -1,2 +1,18 @@
+@echo off
+chcp 65001 > nul
+
+color 06
+title STARDIX BOT by LKzinxYz
+
+mode con: cols=70 lines=15
+echo    ____  ____  __   ____  ____  __  _  _    ____   __  ____  ____
+echo   / ___)(_  _)/ _\ (  _ \(    \(  )( \/ )  (  _ \ /  \(_  _)/ ___)
+echo   \___ \  )( /    \ )   / ) D ( )(  )  (    ) _ ((  O ) )(  \___ \
+echo   (____/ (__)\_/\_/(__\_)(____/(__)(_/\_)  (____/ \__/ (__) (____/
+echo.
+echo                       BOT by LKzinxYz v1.9
+echo                https://github.com/LKzinxyz/SDBOT
+echo.
+
 node index.js
-pause
+pause 
