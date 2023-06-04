02e1255392f5ec36b52b95a5a31407009a4aecb4
LucasLK <122848089+LKzinxyz@users.noreply.github.com>
2023-06-03 22:10:58 -0300

    código arrumado1

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mineflayer.multiple = async (bots, constructor) => {
   if (isMainThread) {
     const threads = [];
     for (const i in bots) {
-      await utils.sleep(6000);
+      await utils.sleep(10000);
       threads.push(new Worker(__filename, { workerData: bots[i] }));
     }
   } else {
@@ -45,66 +45,66 @@ const bot_creator = ({ username, pass, home, auth }) => {
     home
   });
 
-  ready(client);
+  ready(client, username);
 
   client.on('messageCreate', async (message) => {
     if (message.channel.id !== channel.id) return;
     if (!(message.content.startsWith('+'))) return;
-  
+
     handleCommands(bot, channel, username, message);
   });
 
-   // Variable
-   bot.location = 'unknown';
-   bot.isRestarting = false;
-   bot.disconnected = false;
- 
-   // Events
-   bot.once('login', async () => console.log("Conectando > ".brightMagenta + username));
- 
-   bot.on('spawn', async () => {
-     await utils.sleep(1500);
-     await utils.getLocation(bot, home, async () => {
-       if (bot.location === 'home') {
-         channel.send(`${username} chegou na home (/pw${home})`);
-       } else {
-         return;
-       }
-     });
-   });
- 
-   bot.on('message', async (message) => {
-     console.log(message.toAnsi());
-     if (message.toString().includes('Por favor, faça o login')) bot.chat(`/login ${pass}`);
-     else if (message.toString().startsWith('Servidor está reiniciando')) {
-       console.log(`Servidor reiniciando, desconectando: ${bot.username}`.cyan);
-       bot.isRestarting = true;
-       bot.quit();
-     }
-   });
- 
-   bot.on('end', async (reason) => {
-     if (reason.includes('quitting') && bot.isRestarting) {
-       client.removeAllListeners();
-       bot.removeAllListeners();
-       bot._client.removeAllListeners();
- 
-       utils.log(`${username} aguardando 5 min para reconectar`, 'brightMagenta');
-       await utils.sleep(60000 * 5);
-       bot.isRestarting = false;
- 
-       bot_creator({ username, pass, home, auth });
-     } else if (reason.includes('quitting') && bot.disconnected) {
-       console.log('disconnected');
-     } else {
-       client.removeAllListeners();
-       bot.removeAllListeners();
-       bot._client.removeAllListeners();
-       utils.log(`${username} foi desconectado, reconectando...`, 'brightRed');
-       await utils.sleep(7500);
-       bot_creator({ username, pass, home, auth });
-     }
-   });
- };
+  // Variable
+  bot.location = 'unknown';
+  bot.isRestarting = false;
+  bot.disconnected = false;
 
- mineflayer.multiple(accounts, bot_creator);
+  // Events
+  bot.once('login', async () => console.log("Conectando > ".brightMagenta + username));
+
+  bot.on('spawn', async () => {
+    await utils.sleep(1500);
+    await utils.getLocation(bot, home, async () => {
+      if (bot.location === 'home') {
+        channel.send(`${username} chegou na home (/pw${home})`);
+      } else {
+        return;
+      }
+    });
+  });
+
+  bot.on('message', async (message) => {
+    console.log(message.toAnsi());
+    if (message.toString().includes('Por favor, faça o login')) bot.chat(`/login ${pass}`);
+    else if (message.toString().startsWith('Servidor está reiniciando')) {
+      console.log(`Servidor reiniciando, desconectando: ${bot.username}`.cyan);
+      bot.isRestarting = true;
+      bot.quit();
+    }
+  });
+
+  bot.on('end', async (reason) => {
+    if (reason.includes('quitting') && bot.isRestarting) {
+      client.removeAllListeners();
+      bot.removeAllListeners();
+      bot._client.removeAllListeners();
+
+      utils.log(`${username} aguardando 5 min para reconectar`, 'brightMagenta');
+      await utils.sleep(60000 * 5);
+      bot.isRestarting = false;
+
+      bot_creator({ username, pass, home, auth });
+    } else if (reason.includes('quitting') && bot.disconnected) {
+      console.log('disconnected');
+    } else {
+      client.removeAllListeners();
+      bot.removeAllListeners();
+      bot._client.removeAllListeners();
+      utils.log(`${username} foi desconectado, reconectando...`, 'brightRed');
+      await utils.sleep(8500);
+      bot_creator({ username, pass, home, auth });
+    }
+  });
+};
+
+mineflayer.multiple(accounts, bot_creator);
