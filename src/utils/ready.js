a748f35a320c1d23aab8c8e45b02396746c9bc80
LucasLK <122848089+LKzinxyz@users.noreply.github.com>
2023-06-03 21:11:57 -0300

    connect log

--- src/utils/ready.js
+++ src/utils/ready.js
@@ -1,8 +1,10 @@
 const config = require('../../config.json');
 
-module.exports = (client) => {
+module.exports = (client, username) => {
   client.on('ready', () => {
     channel = client.channels.cache.get(config.channelId);
+    channel.send(`Iniciando: ${username}`);
+    console.log(`Iniciando: ${username}`);
     return;
   });
 }
